64c3b15b20344a09d3251e5e298b64b7544501c5
stefanik12 <stefanik.mich@gmail.com>
2024-10-14 16:51:27 +0200

    Training script

--- scripts/distil_pythia_babylm.py
+++ scripts/distil_pythia_babylm.py
@@ -4,6 +4,7 @@
 #  make it running on lumi
 
 import argparse
+import os
 
 import torch
 import transformers
@@ -16,51 +17,66 @@ from evaluators import LMHarnessEvaluator
 from objectives.base_overrides import DistilledCLM, BaselineCLM
 from objectives.soft_clm import SoftCLM
 
-torch.multiprocessing.set_start_method('spawn')
-
 parser = argparse.ArgumentParser()
-parser.add_argument("--objective", help="Objective of training. One of `soft-clm`, `distilled-clm`, `clm`",
+parser.add_argument("--objective",
+                    help="Objective of training. One of `soft-clm`, `distilled-clm`, `clm`, `clm+soft-clm`",
                     required=True, type=str)
 parser.add_argument("--base_model", help="A model to initialize the training with", required=True, type=str)
+parser.add_argument("--output_dir_root", help="A root dir for training checkpoints", required=True, type=str)
 parser.add_argument("--teacher_model", help="Teacher model", type=str, default="")
 parser.add_argument("--batch_size", help="Used batch size", type=int, default=4)
+parser.add_argument("--lr", help="Used batch size", type=int, default=3e-4)
 parser.add_argument("--batch_aggregation", help="Batch aggregation", type=int, default=2)
 parser.add_argument("--eval_steps", help="Eval steps", type=int, default=100)
+parser.add_argument("--train_texts", help="Training texts", type=str)
+parser.add_argument("--val_texts", help="Perplexity validation texts", type=str)
+
+# TODO: this would be nice, but requires nontrivial rewrite of lm_eval.evaluator.simple_evaluate:
+# parser.add_argument("--eval_tasks_root", help="Root directory of lm_eval's evaluation tasks", type=str)
+
 args = parser.parse_args()
 
 # model_path = "EleutherAI/pythia-160m"
 # model_path = "EleutherAI/pythia-14m"
 
 lang_module = LangModule(args.base_model)
-# lang_module = LangModule("facebook/nllb-200-distilled-600M")
-
-
-# lang_module = LangModule("stas/mt5-tiny-random")
-
 
 TrainingObj = SoftCLM if args.objective == "soft-clm" else DistilledCLM if args.objective == "distilled-clm" else BaselineCLM
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
 objective_kwargs = {
     "lang_module": lang_module,
     "batch_size": args.batch_size,
-    "texts_or_path": "data/train_10M/all_shuf_1k_nonempty.train"
-                     if device == "cpu" else "data/train_10M/all_shuf_nonempty.train",
-    "val_texts_or_path": "data/dev/all_shuf_1k_nonempty.dev",
+    "texts_or_path": args.train_texts,
+    "val_texts_or_path": args.val_texts,
     "source_lang_id": "eng_Latn",
     "target_lang_id": "eng_Latn",
     "teacher_model": transformers.AutoModelForCausalLM.from_pretrained(args.teacher_model).to(device)
 }
-if args.objective == "clm":
-    del objective_kwargs["teacher_model"]
-    extra_eval_objectives = []
+
+evaluators = LMHarnessEvaluator(tasks=['blimp_filtered', 'ewok_filtered'], batch_size=args.batch_size,
+                                debug_mode=args.base_model == "EleutherAI/pythia-14m")
+
+train_objectives = []
+extra_eval_objectives = []
+
+if "soft-clm" in args.objective:
+    train_objectives.append(SoftCLM(**objective_kwargs, val_evaluators=[evaluators]))
+elif "distilled-clm" in args.objective:
+    train_objectives.append(DistilledCLM(**objective_kwargs, val_evaluators=[evaluators]))
+
+baseline_kwargs = {k: v for k, v in objective_kwargs.items() if k != "teacher_model"}
+if not train_objectives:
+    # no other training objectives -> avoid duplicate evaluations
+    baseline_kwargs["val_evaluators"] = evaluators
+
+baseline_clm_obj = BaselineCLM(**baseline_kwargs)
+
+if args.objective in ["clm", "clm+soft-clm"]:
+    # training objectives include simple clm
+    train_objectives.append(baseline_clm_obj)
 else:
-    extra_eval_objectives = [BaselineCLM(**{k: v for k, v in objective_kwargs.items() if k != "teacher_model"})]
+    extra_eval_objectives.append(baseline_clm_obj)
 
-evaluators = LMHarnessEvaluator(tasks=['blimp_filtered'])
-
-train_objectives = [TrainingObj(**objective_kwargs, val_evaluators=[evaluators])]
 
 # Add pad token to all models if using pythia
 if train_objectives[0].tokenizer.pad_token is None and train_objectives[0].tokenizer.pad_token_id is None:
@@ -70,20 +86,21 @@ if train_objectives[0].tokenizer.pad_token is None and train_objectives[0].token
 #     train_objectives[0].teacher_model.pad_token = "<|endoftext|>"
 
 
-training_arguments = AdaptationArguments(output_dir="adaptation_output_dir",
+training_arguments = AdaptationArguments(output_dir=os.path.join(args.output_dir_root,
+                                                                 "%s_checkpoints" % "+".join(str(o for o in train_objectives))),
                                          stopping_strategy=StoppingStrategy.ALL_OBJECTIVES_CONVERGED,
                                          do_train=True,
                                          do_eval=True,
                                          gradient_accumulation_steps=args.batch_aggregation,
                                          eval_steps=args.eval_steps,
                                          evaluation_strategy="steps",
-                                         logging_steps=1000,
+                                         logging_steps=100,
                                          num_train_epochs=5,
                                          warmup_steps=1000,
-                                         learning_rate=4e-4,
+                                         learning_rate=args.lr,
                                          save_steps=10000,
-                                         bf16=False if device == "cpu" else False,
-                                         no_cuda=True if device == "cpu" else False,
+                                         bf16=torch.cuda.is_available(),
+                                         no_cuda=not torch.cuda.is_available(),
                                          )
 schedule = ParallelSchedule(train_objectives, training_arguments, extra_eval_objectives)
 
